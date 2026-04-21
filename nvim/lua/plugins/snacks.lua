d45399c9014d47d28e7a1b68cb850c72fcffee80
Yohann <yohann@device-105.home>
2026-04-21 09:27:58 +0200

    Fix snacks

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,8 +1,15 @@
 return {
   "folke/snacks.nvim",
   opts = {
-    explorer = {
-      hidden = true, -- Affiche les fichiers cachés
+    picker = {
+      hidden = true, -- Ne pas afficher le picker dans la liste des fenêtres
+      ignored = true,
+      sources = {
+        files = {
+          hidden = true, -- Ne pas afficher les fichiers cachés dans le picker
+          ignored = true, -- Ne pas afficher les fichiers ignorés (ex: .gitignore) dans le picker
+        },
+      },
     },
   },
 }
